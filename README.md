1adb2632ad67bd3b80ccb8722c9cba7ea23bd15a
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2021-09-25 23:13:36 +0900

    🌱 init

---
+++ README.md
@@ -0,0 +1,2 @@
+# traQ Spotify O2i
+Spotify OGP in traQ to iframe element
---
+++ content_script.js
@@ -0,0 +1,17 @@
+const regex = /^https?:\/\/open\.spotify\.com\/(?<type>[^/]+)\/(?<id>[^/?]+)/
+const main = () => {
+  const spotify_ogps = document.querySelectorAll("[class^=_body]>div>[class^=_messageContents]>[class^=_container] [href^='https://open.spotify.com']")
+  spotify_ogps.forEach(element => {
+    const spotify_url = element.getAttribute('href')
+    const match = spotify_url.match(regex)
+    if (!match) return
+    const {
+      type, id
+    } = match.groups
+    const embed = `https://open.spotify.com/embed/${type}/${id}`
+    const template = document.createElement('template')
+    template.innerHTML = `<iframe src="${embed}" width="100%" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>`
+    element.replaceWith(template.content.firstChild)
+  })
+}
+setInterval(main, 1000)
